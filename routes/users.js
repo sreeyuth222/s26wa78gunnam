8ee4c4cbe8898717b2ded4ebd30eee08f7431bf7
Sreeyuth Chowdary Gunnam <s560337@nwmissouri.edu>
2026-03-20 12:19:07 -0500

    modify users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var vOne = 10;
+var vTwo = 15;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if (vOne > vTwo) {
+    vOne = vOne - vTwo;
+  } else {
+    vTwo = vTwo - vOne;
+  }
+
+  res.send(`Values are: ${vOne} and ${vTwo}`);
 });
 
 module.exports = router;
